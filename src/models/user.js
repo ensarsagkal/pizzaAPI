c15b53de70b3917a5c886397631d3a52ecabbce2
ensar <ensarsagkal@gmail.com>
2024-03-24 14:49:45 +0300

    feat:userModel done

--- src/models/user.js
+++ src/models/user.js
@@ -14,8 +14,30 @@ const UserSchema= new mongoose.Schema(
             required: true,
             unique: true,
           },
+          password: {
+            type: String,
+            trim: true,
+            required: true,
+            set: (password) => passwordEncrypt(password),
+          },
+          email: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: true,
+            validate: (email) => email.includes("@") && email.includes("."),
+          },
+          isActive: {
+            type: Boolean,
+            default: true,
+          },
+          isAdmin: {
+            type: Boolean,
+            default: false,
+          },
     },
     {
         collection:"users",
         timestamps:true
     })
+    module.exports= mongoose.model("User",UserSchema)
