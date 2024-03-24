972b29e60237dbc65b0f2062df2c80b4cda04037
ensar <ensarsagkal@gmail.com>
2024-03-24 14:45:16 +0300

    feat user model start

--- src/models/user.js
+++ src/models/user.js
@@ -4,3 +4,18 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+const passworrEncrypyt=require("../helpers/passwordEncrypt")
+
+const UserSchema= new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: true,
+          },
+    },
+    {
+        collection:"users",
+        timestamps:true
+    })
