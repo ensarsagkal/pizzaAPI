c4dee4959412b874e9ad1ff9aa8c6b85a947d54f
ensar <ensarsagkal@gmail.com>
2024-03-24 15:01:13 +0300

    feat:usercontroller structured

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,3 +2,15 @@
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+const User= require("../models/user")
+module.exports={
+    list:async(req,res)=>{},
+
+    create:async(req,res)=>{},
+
+    read:async(req,res)=>{},
+
+    update:async(req,res)=>{},
+    
+    delete:async(req,res)=>{},
+}
