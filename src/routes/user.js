fc330e6d0bfd0124c195569dd8b1b0d25f424a46
ensar <ensarsagkal@gmail.com>
2024-03-25 22:34:30 +0300

    up

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,6 +4,17 @@
 ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+const user= require("../controllers/user")
 
+router.route("/")
+.get(user.list)
+.post(user.create)
+
+
+router.route("/:id")
+.get(user.read)
+.put(user.update)
+.patch(user.update)
+.delete(user.delete)
 /* ------------------------------------------------------- */
 module.exports = router
