bf004540b83c8c669c48b1bc312524073edf7912
ensar <ensarsagkal@gmail.com>
2024-03-25 23:08:59 +0300

    up

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,3 +2,77 @@
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+// Auth Controller:
+
+const User = require('../models/user')
+const Token = require('../models/token')
+const passwordEncrypt = require('../helpers/passwordEncrypt')
+
+module.exports = {
+
+    login: async (req, res) => {
+        /*
+            #swagger.tags = ["Authentication"]
+            #swagger.summary = "Login"
+            #swagger.description = 'Login with username (or email) and password for get simpleToken and JWT'
+            #swagger.parameters["body"] = {
+                in: "body",
+                required: true,
+                schema: {
+                    "username": "test",
+                    "password": "aA?123456",
+                }
+            }
+        */
+
+        const { username, email, password } = req.body
+
+        if ((username || email) && password) {
+
+            const user = await User.findOne({ $or: [{ username }, { email }] })
+
+            if (user && user.password == passwordEncrypt(password)) {
+
+                if (user.isActive) {
+
+                    /* SIMPLE TOKEN */
+
+                    let tokenData = await Token.findOne({ userId: user.id })
+
+                    if (!tokenData) tokenData = await Token.create({
+                        userId: user.id,
+                        token: passwordEncrypt(user.id + Date.now())
+                    })
+
+                    /* SIMPLE TOKEN */
+
+                    res.status(200).send({
+                        error: false,
+                        token: tokenData.token,
+                        user
+                    })
+
+                } else {
+
+                    res.errorStatusCode = 401
+                    throw new Error('This account is not active.')
+
+                }
+            } else {
+
+                res.errorStatusCode = 401
+                throw new Error('Wrong username/email or password.')
+            }
+        } else {
+
+            res.errorStatusCode = 401
+            throw new Error('Please enter username/email and password.')
+        }
+
+    },
+
+    logout: async (req, res) => {
+        
+    }
+
+}
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,6 +4,16 @@
 ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+// routes/auth:
+
+const auth = require('../controllers/auth')
+
+// URL: /auth
+
+// Login/logout:
+router.post('/login', auth.login)
+// router.all('/logout', auth.logout)
+router.get('/logout', auth.logout)
 
 /* ------------------------------------------------------- */
 module.exports = router
