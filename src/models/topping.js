00c003631e67eb489042454f074e72d41b6e85fc
ensar <ensarsagkal@gmail.com>
2024-03-26 12:19:26 +0300

    up

--- src/models/topping.js
+++ src/models/topping.js
@@ -1,14 +1,24 @@
+"use strict"
+/* -------------------------------------------------------
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
+------------------------------------------------------- */
+const { mongoose } = require('../configs/dbConnection')
+/* ------------------------------------------------------- */
 // Topping Model:
 
 const ToppingSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        trim:true,
-        unique:true,
-        required:true,
+
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
     }
-},{
-    collection:'toppings',
-    timestamps:true
+
+}, {
+    collection: 'toppings',
+    timestamps: true
 })
-module.exports = mongoose.model("Topping",ToppingSchema)
+
+// Model:
+module.exports = mongoose.model('Topping', ToppingSchema)
