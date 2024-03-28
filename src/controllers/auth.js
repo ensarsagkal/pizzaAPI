d7fd51fba800abc6f3844770173f2fe884cd168f
ensar <ensarsagkal@gmail.com>
2024-03-28 14:21:06 +0300

    feat:refresh

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,18 +1,17 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
 // Auth Controller:
 
-const User = require('../models/user')
-const Token = require('../models/token')
-const passwordEncrypt = require('../helpers/passwordEncrypt')
-const jwt = require('jsonwebtoken')
+const User = require("../models/user");
+const Token = require("../models/token");
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
-
-    login: async (req, res) => {
-        /*
+  login: async (req, res) => {
+    /*
             #swagger.tags = ["Authentication"]
             #swagger.summary = "Login"
             #swagger.description = 'Login with username (or email) and password for get simpleToken and JWT'
@@ -26,102 +25,107 @@ module.exports = {
             }
         */
 
-        const { username, email, password } = req.body
+    const { username, email, password } = req.body;
 
-        if ((username || email) && password) {
+    if ((username || email) && password) {
+      const user = await User.findOne({ $or: [{ username }, { email }] });
 
-            const user = await User.findOne({ $or: [{ username }, { email }] })
+      if (user && user.password == passwordEncrypt(password)) {
+        if (user.isActive) {
+          /* SIMPLE TOKEN */
 
-            if (user && user.password == passwordEncrypt(password)) {
+          let tokenData = await Token.findOne({ userId: user.id });
 
-                if (user.isActive) {
+          if (!tokenData)
+            tokenData = await Token.create({
+              userId: user.id,
+              token: passwordEncrypt(user.id + Date.now()),
+            });
 
-                    /* SIMPLE TOKEN */
+          /* SIMPLE TOKEN */
 
-                    let tokenData = await Token.findOne({ userId: user.id })
+          /* JWT */
 
-                    if (!tokenData) tokenData = await Token.create({
-                        userId: user.id,
-                        token: passwordEncrypt(user.id + Date.now())
-                    })
+          const accessInfo = {
+            key: process.env.ACCESS_KEY,
+            time: "30m",
+            data: {
+              id: user.id,
+              username: user.username,
+              email: user.email,
+              password: user.password,
+              isActive: user.isActive,
+              isAdmin: user.isAdmin,
+            },
+          };
 
-                    /* SIMPLE TOKEN */
+          const refreshInfo = {
+            key: process.env.REFRESH_KEY,
+            time: "3d",
+            data: {
+              id: user.id,
+              password: user.password, // encrypted password
+            },
+          };
 
-                    /* JWT */
+          // jwt.sign(data, secret_key, { expiresIn: '30m' })
+          const accessToken = jwt.sign(accessInfo.data, accessInfo.key, {
+            expiresIn: accessInfo.time,
+          });
+          const refreshToken = jwt.sign(refreshInfo.data, refreshInfo.key, {
+            expiresIn: refreshInfo.time,
+          });
 
-                    const accessInfo = {
-                        key: process.env.ACCESS_KEY,
-                        time: '30m',
-                        data: {
-                            id: user.id,
-                            username: user.username,
-                            email: user.email,
-                            password: user.password,
-                            isActive: user.isActive,
-                            isAdmin: user.isAdmin,
-                        },
-                    }
+          /* JWT */
 
-                    const refreshInfo = {
-                        key: process.env.REFRESH_KEY,
-                        time: '3d',
-                        data: {
-                            id: user.id,
-                            password: user.password // encrypted password
-                        },
-                    }
-
-                    // jwt.sign(data, secret_key, { expiresIn: '30m' })
-                    const accessToken = jwt.sign(accessInfo.data, accessInfo.key, { expiresIn: accessInfo.time })
-                    const refreshToken = jwt.sign(refreshInfo.data, refreshInfo.key, { expiresIn: refreshInfo.time })
-
-                    /* JWT */
-
-                    res.status(200).send({
-                        error: false,
-                        token: tokenData.token,
-                        bearer: {
-                            access: accessToken,
-                            refresh: refreshToken,
-                        },
-                        user
-                    })
-
-                } else {
-
-                    res.errorStatusCode = 401
-                    throw new Error('This account is not active.')
-
-                }
-            } else {
-
-                res.errorStatusCode = 401
-                throw new Error('Wrong username/email or password.')
-            }
+          res.status(200).send({
+            error: false,
+            token: tokenData.token,
+            bearer: {
+              access: accessToken,
+              refresh: refreshToken,
+            },
+            user,
+          });
         } else {
-
-            res.errorStatusCode = 401
-            throw new Error('Please enter username/email and password.')
+          res.errorStatusCode = 401;
+          throw new Error("This account is not active.");
         }
+      } else {
+        res.errorStatusCode = 401;
+        throw new Error("Wrong username/email or password.");
+      }
+    } else {
+      res.errorStatusCode = 401;
+      throw new Error("Please enter username/email and password.");
+    }
+  },
+  refresh: async (req, res) => {
+    const refreshToken = req.body?.bearer?.refresh;
+    if (refreshToken) {
+        const refreshData= await jwt.verify(refreshToken,process.env.REFRESH_KEY)
+        
+    }else{
+        res.errorStatusCode=401
+        throw new Error("Please enter bearer.refresh")
+    }
+  },
 
-    },
-
-    logout: async (req, res) => {
-        /*
+  logout: async (req, res) => {
+    /*
             #swagger.tags = ["Authentication"]
             #swagger.summary = "simpleToken: Logout"
             #swagger.description = 'Delete token key.'
         */
 
-        const auth = req.headers?.authorization // Token ...tokenKey...
-        const tokenKey = auth ? auth.split(' ') : null // ['Token', '...tokenKey...']
-        const result = await Token.deleteOne({ token: tokenKey[1] })
+    const auth = req.headers?.authorization; // Token ...tokenKey...
+    const tokenKey = auth ? auth.split(" ") : null; // ['Token', '...tokenKey...']
+    const result = await Token.deleteOne({ token: tokenKey[1] });
 
-        res.send({
-            error: false,
-            message: 'Token deleted. Logout was OK.',
-            result
-        })
-    }
-
-}
+    res.send({
+      error: false,
+      message: "Token deleted. Logout was OK.",
+      result,
+    });
+  },
+};
