c2c0aedfbeba4890d780da9f391af01df8925654
ensar <ensarsagkal@gmail.com>
2024-03-28 22:01:11 +0300

    send mail

--- index.js
+++ index.js
@@ -60,25 +60,40 @@ const nodemailer= require("nodemailer")
   pop3: { host: 'pop3.ethereal.email', port: 995, secure: true },
   web: 'https://ethereal.email'
 */
-const transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false,
-    auth:{
-        user: 'asjxwssdyx3bwaqk@ethereal.email',
-        pass: 'tp8f9agrT14Dyqs7sr'   
+// const transporter = nodemailer.createTransport({
+//     host: "smtp.ethereal.email",
+//     port: 587,
+//     secure: false,
+//     auth:{
+//         user: 'asjxwssdyx3bwaqk@ethereal.email',
+//         pass: 'tp8f9agrT14Dyqs7sr'   
 
-    }
-})
-transporter.sendMail({
-    from: 'sfystdx7sif4vdr3@ethereal.email',
-    to: 'ensarsagkal@gmail.com', // 'a@b.com, c@d.com'
-    subject: 'Hello',
-    text: 'Hello There. How are you?',
-    html: '<b>Hello There.</b> <p>How are you?</p>',
-}, (error, success) => {
-    success ? console.log('SUCCESS', success) : console.log('ERROR', error)
-})
+//     }
+// })
+// transporter.sendMail({
+//     from: 'sfystdx7sif4vdr3@ethereal.email',
+//     to: 'ensarsagkal@gmail.com', // 'a@b.com, c@d.com'
+//     subject: 'Hello',
+//     text: 'Hello There. How are you?',
+//     html: '<b>Hello There.</b> <p>How are you?</p>',
+// }, (error, success) => {
+//     success ? console.log('SUCCESS', success) : console.log('ERROR', error)
+// })
+// //* Google -> AccountHome -> Security -> Two-Step-Verify -> App-Passwords
+
+// const transporter=nodemailer.createTransport({
+//     service:"gmail",
+//     auth:{
+//         user:"ensarsagkal@gmail.com",
+//         pass:"ypop huad lwqc liih"
+//     }
+// })
+// transporter.sendMail({
+//     // to:"qadiradamson@gmail.com",
+//         subject: 'Hello',
+//     text: 'Hello There. How are you?',
+//     html: '<b>Hello There.</b> <p>How are you?</p>',
+// },(error, succes)=>console.log(succes,error))
 /* ------------------------------------------------------- */
 
 
