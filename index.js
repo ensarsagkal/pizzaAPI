635a62ccbc752f6141bc83bccffc0912c8b831f9
ensar <ensarsagkal@gmail.com>
2024-03-28 21:36:32 +0300

    mail

--- index.js
+++ index.js
@@ -60,7 +60,25 @@ const nodemailer= require("nodemailer")
   pop3: { host: 'pop3.ethereal.email', port: 995, secure: true },
   web: 'https://ethereal.email'
 */
+const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false,
+    auth:{
+        user: 'asjxwssdyx3bwaqk@ethereal.email',
+        pass: 'tp8f9agrT14Dyqs7sr'   
 
+    }
+})
+transporter.sendMail({
+    from: 'sfystdx7sif4vdr3@ethereal.email',
+    to: 'ensarsagkal@gmail.com', // 'a@b.com, c@d.com'
+    subject: 'Hello',
+    text: 'Hello There. How are you?',
+    html: '<b>Hello There.</b> <p>How are you?</p>',
+}, (error, success) => {
+    success ? console.log('SUCCESS', success) : console.log('ERROR', error)
+})
 /* ------------------------------------------------------- */
 
 
