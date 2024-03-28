a4c111251c8853f0af06d4f27dfe3b89eea5e198
ensar <ensarsagkal@gmail.com>
2024-03-28 15:38:27 +0300

    feat:t toppings router permissions added

--- src/routes/topping.js
+++ src/routes/topping.js
@@ -7,9 +7,10 @@ const router = require('express').Router()
 // routes/topping:
 
 const topping = require('../controllers/topping')
+const {isAdmin}=require("../middlewares/permissions")
 
 // URL: /toppings
-
+router.use(isAdmin)
 router.route('/')
     .get(topping.list)
     .post(topping.create)
